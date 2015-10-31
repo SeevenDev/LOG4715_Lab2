574d7a9fd2a918811999318ba51a55439456d1a1
Seeven <seeven.dev@gmail.com>
2015-10-30 23:09:34 -0400

    Mec2. Carapace Verte (explosion)
    - Ajout de l'effet d'exlosion au contact d'une voiture (reste à
    "pousser" les voitures à l'explosion avec un SphereOverlap).

---
+++ Jeu de course/Assets/Cadriciel/Scripts/ProjectileCollider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileCollider : MonoBehaviour 
+{
+	private GameObject explosion;
+
+	// Use this for initialization
+	void Start () 
+	{
+		explosion = Resources.Load ("Fireworks") as GameObject;
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		string rootName = collision.transform.root.name;
+		if (rootName == "Cars") 
+		{
+			// Effet visuel de l'explosion : 
+			GameObject fx = Instantiate (explosion) as GameObject;
+			fx.transform.position = transform.position; 
+			Destroy (fx, 1);
+
+			// Détruire la carapace :
+			Destroy (gameObject, 1);
+		}
+	}
+}
