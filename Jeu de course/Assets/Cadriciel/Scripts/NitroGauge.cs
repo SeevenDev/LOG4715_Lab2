bdebdf3797cb3afcc82754879e327cf15762ca5e
Mehdi-H <mehdi.houacine@edu.esiee.fr>
2015-11-08 14:06:14 -0500

    Ajustement de la hauteur des booster et de la nitro :car:
    Les booster sont placés un peu plus bas pour éviter un effet de bump, et
    la nitro est consommé un peu moins rapidement.

--- Jeu de course/Assets/Cadriciel/Scripts/NitroGauge.cs
+++ Jeu de course/Assets/Cadriciel/Scripts/NitroGauge.cs
@@ -20,17 +20,15 @@ public class NitroGauge : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		Debug.Log (progress);
-
 		if (progress >= 100)
 			progress = 100;
 
 		if (progress <= 0)
 			progress = 0;
 
-		if (Input.GetKey ("x") && progress > 50) {
+		if (Input.GetKey ("x") && progress > 30) {
 			transform.rigidbody.AddForce (transform.forward * nitroForce);
-			progress -= 5;
+			progress -= 3;
 		}
 
 		if (Input.GetKey ("x") && progress <= 50) {
