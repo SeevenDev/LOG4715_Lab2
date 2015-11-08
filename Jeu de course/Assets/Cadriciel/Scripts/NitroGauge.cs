b30e7350bd21308df48c583709ccfc1d91ecd468
Mehdi-H <mehdi.houacine@edu.esiee.fr>
2015-11-07 21:22:36 -0500

    [Méca 11] Nitro et jauge de nitro
    Le joueur peut utiliser du nitro en temps limité en appuyant sur la
    touche x. La jauge remonte toute seule, lentement pour éviter les abus.

---
+++ Jeu de course/Assets/Cadriciel/Scripts/NitroGauge.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NitroGauge : MonoBehaviour {
+
+	[SerializeField]
+	private float progress = 100;
+
+	[SerializeField]
+	private float nitroForce = 100;
+
+	[SerializeField]
+	private GUITexture _nitro;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log (progress);
+
+		if (progress >= 100)
+			progress = 100;
+
+		if (progress <= 0)
+			progress = 0;
+
+		if (Input.GetKey ("x") && progress > 50) {
+			transform.rigidbody.AddForce (transform.forward * nitroForce);
+			progress -= 5;
+		}
+
+		if (Input.GetKey ("x") && progress <= 50) {
+			progress -= 2;
+		}
+
+
+		progress += 0.5f;
+
+		_nitro.pixelInset = new Rect(_nitro.pixelInset.x, _nitro.pixelInset.y, progress, _nitro.pixelInset.height);
+	}
+
+}
+
--- Jeu de course/Assets/Cadriciel/Scripts/SpeedBooster.cs
+++ Jeu de course/Assets/Cadriciel/Scripts/SpeedBooster.cs
@@ -21,23 +21,9 @@ public class SpeedBooster : MonoBehaviour {
 		if(col.gameObject.transform.parent.name == "Cars")
 		{
 			GameObject joueur = col.gameObject;
-			Debug.Log(joueur.name);
 			Vector3 originalSpeed = joueur.transform.rigidbody.velocity;
-			Debug.Log(originalSpeed);
 			joueur.transform.rigidbody.AddForce(joueur.transform.forward * boostForce);
-			//StartCoroutine(TemporarySpeedUp(col, originalSpeed));
 		}
 	}
-
-//	IEnumerator TemporarySpeedUp(Collision col ,Vector3 originalSpeed){
-//		yield return new WaitForSeconds(4);
-//
-//		Vector3 vitesseJoueur = col.gameObject.transform.rigidbody.velocity;
-//		GameObject joueur = col.gameObject;
-//
-//		//while(vitesseJoueur.z > originalSpeed.z)
-//		//	joueur.transform.rigidbody.AddForce( - joueur.transform.forward * boostForce/10);
-//		joueur.transform.rigidbody.velocity = originalSpeed;
-//	}
 	
 }
