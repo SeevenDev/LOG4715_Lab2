566e6b5b3fcb4d32eb75dcbd4d2ff2f8b3afd738
Seeven <seeven.dev@gmail.com>
2015-11-08 22:17:38 -0500

    Mec10. Collectionnables (healer fini)
    - Ajout de l'objet Healer et du script Healer.cs.
    - Disposition de plusieurs Healers sur le terrain.
    - Les Healers réapparaissent au bout de 30 secondes et soignent de 30
    PV.

---
+++ Jeu de course/Assets/Cadriciel/Scripts/Healer.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class Healer : MonoBehaviour
+{
+	// ==========================================
+	// == Attributs
+	// ==========================================
+
+	[SerializeField]
+	private int _healAmount = 30;
+
+	[SerializeField]
+	private int _respawnTimeMs = 30000;
+
+	private Stopwatch _count;
+	private Vector3 rotationsPerMinute = new Vector3(10.0f, 10.0f, 10.0f);
+
+	// ==========================================
+	// == Start
+	// ==========================================
+
+	void Start () {
+		_count = new Stopwatch();
+	}
+
+	// ==========================================
+	// == Update
+	// ==========================================
+
+	void Update ()
+	{
+		// === Faire réapparaitre le bloc si besoin ===
+
+		if (!renderer.enabled && _count.ElapsedMilliseconds > _respawnTimeMs)
+		{
+			renderer.enabled = true;
+			_count.Stop();
+			_count.Reset();
+		}
+
+		// === Faire tourner le bloc ===
+
+		if (renderer.enabled)
+		{
+			transform.Rotate(
+				(float)(6.0 * rotationsPerMinute.x * Time.deltaTime),
+				(float)(6.0 * rotationsPerMinute.y * Time.deltaTime),
+				(float)(6.0 * rotationsPerMinute.z * Time.deltaTime)
+			);
+		}
+	}
+
+	// ==========================================
+	// == Trigger
+	// ==========================================
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (renderer.enabled && col.transform.root.name == "Cars")
+		{
+			// Destruction du bloc :
+			renderer.enabled = false;
+			_count.Start();
+
+			// Soin du véhicule :
+			col.transform.parent.parent.GetComponent<Vie>().addHealth(_healAmount);
+		}
+	}
+}
