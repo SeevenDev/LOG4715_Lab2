579ccf243065d9c2d18f9d4f1b381e383205e4b7
Seeven <seeven.dev@gmail.com>
2015-11-08 23:16:14 -0500

    :lollipop: Mec12. Saut (fini)
    - Ajout du script Jump.cs
    - La voiture fait un saut simple avec la barre Espace si elle touche le
    sol.

---
+++ Jeu de course/Assets/Cadriciel/Scripts/Jump.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour
+{
+	// ==========================================
+	// == Attributs
+	// ==========================================
+
+	[SerializeField]
+	private float _jumpForce = 20.0f;
+
+	private bool grounded;
+
+	// ==========================================
+	// == FixedUpdate
+	// ==========================================
+
+	void FixedUpdate ()
+	{
+		// === Sauter ===
+
+		if (Input.GetButtonDown("Jump") && grounded)
+		{
+			rigidbody.AddForce(new Vector3(0, _jumpForce, 0), ForceMode.Impulse);
+		}
+    }
+
+	// ==========================================
+	// == Vérifier si le joueur est au sol
+	// ==========================================
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Track")
+		{
+			grounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision col)
+	{
+		if (col.gameObject.name == "Track")
+		{
+			grounded = false;
+		}
+	}
+}
